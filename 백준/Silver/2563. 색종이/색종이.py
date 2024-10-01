5a4fa54386601850a3b467af67eb57c90610b89c
Jiho Lee <89183883+jeho1129@users.noreply.github.com>
2024-10-01 19:19:39 +0900

    [Silver V] Title: 색종이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,14 +1,15 @@
 import sys
 input = sys.stdin.readline
-
-paper = [[0] * 100 for i in range(100)]
-T = int(input())
-for test_case in range(T):
+N = [[0] * 100 for _ in range(100)]
+X = int(input())
+for i in range(X):
     A, B = map(int, input().split())
-    for i in range(A, A + 10):
-        for j in range(B, B + 10):
-            paper[i][j] = 1
+    for j in range(A - 1, A + 9):
+        for k in range(B - 1, B + 9):
+            N[j][k] = 1
 result = 0
 for i in range(100):
-    result += sum(paper[i])
+    for j in range(100):
+        if N[i][j] == 1:
+            result += 1
 print(result)
