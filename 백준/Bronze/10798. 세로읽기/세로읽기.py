cf9b3bdf60517c9bce0dfd1c3e59a0f11e44f1da
Jiho Lee <89183883+jeho1129@users.noreply.github.com>
2024-10-01 19:11:03 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,11 +1,7 @@
-T = 0
-input_list = []
-for i in range(5):
-    X = list(map(str, input()))
-    if T < len(X):
-        T = len(X)
-    input_list.append(X)
-for i in range(T):
+import sys
+input = sys.stdin.readline
+N = [list(map(str, input().rstrip())) for _ in range(5)]
+for i in range(15):
     for j in range(5):
-        if len(input_list[j]) >= i + 1:
-            print(input_list[j][i], end='')
+        if len(N[j]) > i:
+            print(N[j][i], end='')
