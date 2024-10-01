705f38575409be49af6b07b1c88ce3401d4ae372
Jiho Lee <89183883+jeho1129@users.noreply.github.com>
2024-10-01 19:02:49 +0900

    [Bronze III] Title: 최댓값, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
-
-max_num = 0
+N = [list(map(int, input().split())) for _ in range(9)]
+num = -1
 result = [0, 0]
 for i in range(9):
-    X = list(map(int, input().split()))
     for j in range(9):
-        if X[j] >= max_num:
-            max_num = X[j]
+        if N[i][j] > num:
+            num = N[i][j]
             result = [i + 1, j + 1]
-print(max_num)
+print(num)
 print(*result)
