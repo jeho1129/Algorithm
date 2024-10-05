e28d3e2f591223fb8e4e06ec894a24bd3c8fd4e9
Jiho Lee <89183883+jeho1129@users.noreply.github.com>
2024-10-05 18:44:44 +0900

    [Bronze I] Title: 최소공배수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for i in range(T):
+    A, B = map(int, input().split())
+    result = A * B
+    while B > 0:
+        A, B = B, A % B
+    print(result // A)
