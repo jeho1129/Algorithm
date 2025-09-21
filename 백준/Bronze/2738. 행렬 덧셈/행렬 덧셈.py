6b7b99a36d7d3004ca98f9de4034ee57aef8224b
Jiho Lee <89183883+jeho1129@users.noreply.github.com>
2025-09-21 19:57:00 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,9 +1,9 @@
-import sys
-input = sys.stdin.readline
 N, M = map(int, input().split())
-A = [list(map(int, input().split())) for i in range(N)]
-B = [list(map(int, input().split())) for i in range(N)]
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+result = [[0] * M for _ in range(N)]
 for i in range(N):
     for j in range(M):
-        A[i][j] = A[i][j] + B[i][j]
-    print(*A[i])
+        result[i][j] = A[i][j] + B[i][j]
+for i in range(N):
+    print(*result[i])
